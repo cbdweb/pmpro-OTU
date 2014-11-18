3c3ffe51cd172dfe17d9228f8bf4244185f9bfab
cbdweb <nik@cbdweb.net>
2014-11-18 19:22:23 +1100

    version number obscure and name

--- paid-memberships-pro.php
+++ paid-memberships-pro.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: Paid Memberships Pro
+Plugin Name: Paid Memberships Pro OTU
 Plugin URI: http://www.paidmembershipspro.com
 Description: Plugin to Handle Memberships
-Version: 1.7.15
+taken from: 1.7.15
 Author: Stranger Studios
 Author URI: http://www.strangerstudios.com
 */
