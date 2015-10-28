d24830f4c8f356e5284e7aa4d41428d060933e50
cbdweb <nik@cbdweb.net>
2015-10-28 16:16:50 +1100

    bug in move css to footer

--- includes/init.php
+++ includes/init.php
@@ -37,24 +37,24 @@ function pmpro_init()
 	}
 	else
 	{		
-		$frontend_css_rtl = false;
-		if(file_exists(get_stylesheet_directory() . "/paid-memberships-pro/css/frontend.css")) {
-			$frontend_css = get_stylesheet_directory_uri() . "/paid-memberships-pro/css/frontend.css";
-			if( is_rtl() && file_exists(get_stylesheet_directory() . "/paid-memberships-pro/css/frontend-rtl.css") ) {
-				$frontend_css_rtl = get_stylesheet_directory_uri() . "/paid-memberships-pro/css/frontend-rtl.css";
-			}
-		} elseif(file_exists(get_template_directory() . "/paid-memberships-pro/frontend.css")) {
-			$frontend_css = get_template_directory_uri() . "/paid-memberships-pro/frontend.css";
-			if( is_rtl() && file_exists(get_template_directory() . "/paid-memberships-pro/css/frontend-rtl.css") ) {
-				$frontend_css_rtl = get_template_directory_uri() . "/paid-memberships-pro/css/frontend-rtl.css";
-			}
-		} else {
-			$frontend_css = plugins_url('css/frontend.css',dirname(__FILE__) );	
-			if( is_rtl() ) {
-				$frontend_css_rtl = plugins_url('css/frontend-rtl.css',dirname(__FILE__) );
-			}
-		}
-                add_action( 'wp_footer', function() { // cbdweb move css load to footer
+		add_action( 'wp_footer', function() { // cbdweb move css load to footer
+                    $frontend_css_rtl = false;
+                    if(file_exists(get_stylesheet_directory() . "/paid-memberships-pro/css/frontend.css")) {
+                            $frontend_css = get_stylesheet_directory_uri() . "/paid-memberships-pro/css/frontend.css";
+                            if( is_rtl() && file_exists(get_stylesheet_directory() . "/paid-memberships-pro/css/frontend-rtl.css") ) {
+                                    $frontend_css_rtl = get_stylesheet_directory_uri() . "/paid-memberships-pro/css/frontend-rtl.css";
+                            }
+                    } elseif(file_exists(get_template_directory() . "/paid-memberships-pro/frontend.css")) {
+                            $frontend_css = get_template_directory_uri() . "/paid-memberships-pro/frontend.css";
+                            if( is_rtl() && file_exists(get_template_directory() . "/paid-memberships-pro/css/frontend-rtl.css") ) {
+                                    $frontend_css_rtl = get_template_directory_uri() . "/paid-memberships-pro/css/frontend-rtl.css";
+                            }
+                    } else {
+                            $frontend_css = plugins_url('css/frontend.css',dirname(__FILE__) );	
+                            if( is_rtl() ) {
+                                    $frontend_css_rtl = plugins_url('css/frontend-rtl.css',dirname(__FILE__) );
+                            }
+                    }
                     wp_enqueue_style('pmpro_frontend', $frontend_css, array(), PMPRO_VERSION, "screen");
                     if( $frontend_css_rtl ) {
                             wp_enqueue_style('pmpro_frontend_rtl', $frontend_css_rtl, array(), PMPRO_VERSION, "screen");
